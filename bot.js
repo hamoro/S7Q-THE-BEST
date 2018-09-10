77c31580684e0897b433df3e0a974767f0de7409
hamoro <40411863+hamoro@users.noreply.github.com>
2018-09-10 13:25:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+**
+السلام عليكم
+:earth_asia: حياك الله تنورنه في كلان الاسطير سحق:heart:  ,   https://discord.gg/U2D6C9B
+حلمنه نوصل 200 عضو**
+ [${member}] .... الدعوه خاصه لك يا قلبي .
+**`)
+}).catch(console.error)
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+**
+السلام عليكم
+:earth_asia: حياك الله تنورنه في كلان الاسطير سحق:heart:  ,   https://discord.gg/U2D6C9B
+حلمنه نوصل 200 عضو**
+[${member}]د .... الدعوه خاصه لك يا قلبي .
+**`) 
+}).catch(console.error)
+})
+
 
 
 
